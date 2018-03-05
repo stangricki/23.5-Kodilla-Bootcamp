657da96c1bdf4b1124f8e75a3db869dfdfb3b9bf
Filip Stangricki <stangricki@gmail.com>
2018-03-05 08:57:10 +0100

    Fighting with frontend-backend integration

--- client/modules/Lane/LaneActions.js
+++ client/modules/Lane/LaneActions.js
@@ -1,10 +1,14 @@
 import uuid from 'uuid';
+import callApi from '../../util/apiCaller';
+import { lanes } from '../../util/schema';
+import { normalize } from 'normalizr';
 
 // Export Constants
 export const CREATE_LANE = 'CREATE_LANE';
 export const UPDATE_LANE = 'UPDATE_LANE';
 export const DELETE_LANE = 'DELETE_LANE';
 export const EDIT_LANE = 'EDIT_LANE';
+export const CREATE_LANES = 'CREATE_LANES';
 
 // Export Actions
 
@@ -13,6 +17,7 @@ export function createLane(lane) {
     type: CREATE_LANE,
     lane: {
       id: uuid(),
+      notes: [],
       ...lane,
     }
   };
@@ -36,5 +41,32 @@ export function editLane(laneId) {
   return {
     type: EDIT_LANE,
     laneId,
+  };
+}
+
+export function fetchLanes() {
+  return (dispatch) => {
+    return callApi('lanes').then(res => {
+      const normalized = normalize(res.lanes, lanes);
+      const { lanes: normalizedLanes, notes } = normalized.entities;
+
+     dispatch(createLanes(normalizedLanes));
+     dispatch(createNotes(notes));
+    });
+  };
+}
+
+export function createLanes(lanesData) {
+  return {
+    type: CREATE_LANES,
+    lanes: lanesData,
+  };
+}
+
+export function createLaneRequest(lane) {
+  return (dispatch) => {
+    return callApi('lanes', 'post', lane).then(res => {
+      dispatch(createLane(res));
+    });
   };
 }
